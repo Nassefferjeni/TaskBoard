e4d7098e01d9cc92592f883c2371aae89d04997c
Nasef <nassef.ferjeni@gmail.com>
2024-02-26 21:47:53 +0100

    Form Components

---
+++ components/form/form-submit.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { useFormStatus } from "react-dom";
+import { Button } from "@/components/ui/button";
+
+interface FormSubmitProps {
+  children: React.ReactNode;
+  disabled?: boolean;
+  className?: string;
+  variant?:
+    | "default"
+    | "primary"
+    | "secondary"
+    | "destructive"
+    | "outline"
+    | "ghost"
+    | "link";
+}
+
+export const FormSubmit = ({
+  children,
+  disabled,
+  className,
+  variant,
+}: FormSubmitProps) => {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      type="submit"
+      size="sm"
+      disabled={pending || disabled}
+      className={cn(className)}
+      variant={variant}
+    >
+      {children}
+    </Button>
+  );
+};
